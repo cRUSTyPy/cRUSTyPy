84a4ddc374e6cc5e26395e855ece4bacce964745
Cameron Dershem <cldershem@gmail.com>
2017-05-22 18:01:17 -0400

    reads pyc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,41 @@
 use std::process::Command;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::io::Error;
+
 
 fn main() {
+    compile_bytecode();
+    let bytecode = read_bytecode().unwrap();
+    println!("{:?}", bytecode)
+}
+
+fn compile_bytecode() {
     Command::new("python").arg("-m").arg("main.py").output().expect("fail");
+}
+
+
+fn read_bytecode() -> Result<Vec<u8>, Error> {
+    let mut file = File::open("main.pyc")?;
+
+    let mut meta = vec![0, 8];
+    file.read_exact(&mut meta);
+    // println!("{:?}", meta);
+
+    let mut bytecode = Vec::new();
+    file.read_to_end(&mut bytecode)?;
+
+    Ok(bytecode)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::read_bytecode;
+
+    // #[test]
+    // fn test_bytes() {
+    //     assert_eq!(read_bytecode())
+    // }
 }
